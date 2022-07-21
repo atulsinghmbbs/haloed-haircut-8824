bf52f8a49e4fe3f914bf6f01ff936c4974229d23
atulsinghmbbs <103641560+atulsinghmbbs@users.noreply.github.com>
2022-07-21 18:11:14 +0530

    added init

---
+++ Script/newlyst.js
@@ -0,0 +1,39 @@
+// let form = document.querySelector("form");
+// let signupLS = JSON.parse(localStorage.getItem("signup"))||[];
+
+// form.addEventListener("submit",function(event) {
+//     event.preventDefault();
+
+//     let obj = {
+//         email: form.email.value,
+//         password: form.password.value,
+//     }
+
+//     if ( obj.email == "" || obj.password == "") {
+//         alert("field empty")
+//         return;
+//     }
+//     else {
+//         signupLS.push(obj);
+//         localStorage.setItem("signup", JSON.stringify(signupLS))
+//     }
+ 
+// })
+
+// function saveData(event){
+//     event.preventDefault();
+//     console.log("inside")
+//     let obj = {
+//         email: form.email.value,
+//         password: form.password.value,
+//     }
+
+//     if ( obj.email == "" || obj.password == "") {
+//         alert("field empty")
+//         return;
+//     }
+//     else {
+//         signupLS.push(obj);
+//         localStorage.setItem("signup", JSON.stringify(signupLS))
+//     }
+// }
---
+++ Script/womens.js
@@ -0,0 +1,213 @@
+let product_data= [
+  {
+    name: "NASTY GAL",
+    desc: "Embellised Sequin fringe Trim mini Dress-Black",
+    img: "https://cdna.lystit.com/200/250/tr/photos/nastygal/c6b40cbb/nasty-gal-designer-black-Embellished-Sequin-Fringe-Trim-Bodycon-Mini-Dress.jpeg",
+    price: "$213 $149 (30% off)",
+    logo: "Nasty Gal",
+  },
+  {
+    name: "NASTY GAL",
+    desc: "Cowl Wrap Satin Wrap Maxi Dress-Multicolor",
+    img: "https://cdna.lystit.com/200/250/tr/photos/nastygal/f127a618/nasty-gal-designer-champagne-Cowl-Back-Satin-Wrap-Maxi-Dress.jpeg",
+    price: "$100 $70 (30% off)",
+    logo: "Nasty Gal",
+  },
+  {
+    name: "VERSACE",
+    desc: "Medusa Band V-neck Bra-Black",
+    img: "https://cdna.lystit.com/200/250/tr/photos/cettire/de080468/versace-Black-Medusa-Band-V-neck-Bra.jpeg",
+    price: "$98 $75 (20% off)",
+    logo: "VERSACE",
+  },
+  {
+    name: "NASTY GAL",
+    desc: "Warehouse Sequin Halter Drape midi Dress-Green ",
+    img: "https://cdna.lystit.com/200/250/tr/photos/nastygal/a588672c/nasty-gal-designer-green-Warehouse-Sequin-Halter-Drape-Back-Midi-Dress.jpeg",
+    price: "$121 $85 (30% off)",
+    logo: "VERSACE",
+  },
+  {
+    name: "AGENT PROVOCATEUR",
+    desc: "lorna Bra Multicolor",
+    img: "https://cdna.lystit.com/200/250/tr/photos/ssense/10b6d397/agent-provocateur-BluePink-Lorna-Bra.jpeg",
+    price: "$117",
+    logo: "SSENSE",
+  },
+  {
+    name: "NASTY GAL",
+    desc: "Sequin Embellished Strappy Tulle Maxi Dress-Natural",
+    img: "https://cdna.lystit.com/200/250/tr/photos/nastygal/b7f02410/nasty-gal-designer-beige-Sequin-Embellished-Strappy-Tulle-Maxi-Dress.jpeg",
+    price: "$355 $143 (60% off)",
+    logo: "Nasty Gal",
+  },
+  {
+    name: "VERSACE",
+    desc: "Logo Band Cheeky Thongs-Black",
+    img: "https://cdna.lystit.com/200/250/tr/photos/cettire/f77d741c/versace-Black-Logo-Band-Cheeky-Thongs.jpeg",
+    price: "$51.88 $40 (20% off)",
+    logo: "Cettire",
+  },
+  {
+    name: "NASTY GAL",
+    desc: "Satin Ballon Sleeve Wrap Maxi Dress Green",
+    img: "https://cdna.lystit.com/200/250/tr/photos/nastygal/07d0e3b7/nasty-gal-designer-green-Satin-Balloon-Sleeve-Wrap-Maxi-Dress.jpeg",
+    price: "$94 $66 (30% off)",
+    logo: "Nasty Gal",
+  },
+  {
+    name: "NASTY GAL",
+    desc: "Satin Ballon Sleeve Wrap Maxi Dress Green",
+    img: "https://cdna.lystit.com/200/250/tr/photos/nastygal/07d0e3b7/nasty-gal-designer-green-Satin-Balloon-Sleeve-Wrap-Maxi-Dress.jpeg",
+    price: "$94 $66 (30% off)",
+    logo: "Nasty Gal",
+  },
+  {
+    name: "NASTY GAL",
+    desc: "Embellised Sequin fringe Trim mini Dress-Black",
+    img: "https://cdna.lystit.com/200/250/tr/photos/nastygal/c6b40cbb/nasty-gal-designer-black-Embellished-Sequin-Fringe-Trim-Bodycon-Mini-Dress.jpeg",
+    price: "$213 $149 (30% off)",
+    logo: "Nasty Gal",
+  },
+  {
+    name: "NASTY GAL",
+    desc: "Cowl Wrap Satin Wrap Maxi Dress-Multicolor",
+    img: "https://cdna.lystit.com/200/250/tr/photos/nastygal/f127a618/nasty-gal-designer-champagne-Cowl-Back-Satin-Wrap-Maxi-Dress.jpeg",
+    price: "$100 $70 (30% off)",
+    logo: "Nasty Gal",
+  },
+  {
+    name: "VERSACE",
+    desc: "Medusa Band V-neck Bra-Black",
+    img: "https://cdna.lystit.com/200/250/tr/photos/cettire/de080468/versace-Black-Medusa-Band-V-neck-Bra.jpeg",
+    price: "$98 $75 (20% off)",
+    logo: "VERSACE",
+  },
+  {
+    name: "NASTY GAL",
+    desc: "Warehouse Sequin Halter Drape midi Dress-Green ",
+    img: "https://cdna.lystit.com/200/250/tr/photos/nastygal/a588672c/nasty-gal-designer-green-Warehouse-Sequin-Halter-Drape-Back-Midi-Dress.jpeg",
+    price: "$121 $85 (30% off)",
+    logo: "VERSACE",
+  },
+  {
+    name: "AGENT PROVOCATEUR",
+    desc: "lorna Bra Multicolor",
+    img: "https://cdna.lystit.com/200/250/tr/photos/ssense/10b6d397/agent-provocateur-BluePink-Lorna-Bra.jpeg",
+    price: "$117",
+    logo: "SSENSE",
+  },
+  {
+    name: "NASTY GAL",
+    desc: "Sequin Embellished Strappy Tulle Maxi Dress-Natural",
+    img: "https://cdna.lystit.com/200/250/tr/photos/nastygal/b7f02410/nasty-gal-designer-beige-Sequin-Embellished-Strappy-Tulle-Maxi-Dress.jpeg",
+    price: "$355 $143 (60% off)",
+    logo: "Nasty Gal",
+  },
+  {
+    name: "VERSACE",
+    desc: "Logo Band Cheeky Thongs-Black",
+    img: "https://cdna.lystit.com/200/250/tr/photos/cettire/f77d741c/versace-Black-Logo-Band-Cheeky-Thongs.jpeg",
+    price: "$51.88 $40 (20% off)",
+    logo: "Cettire",
+  },
+  {
+    name: "NASTY GAL",
+    desc: "Satin Ballon Sleeve Wrap Maxi Dress Green",
+    img: "https://cdna.lystit.com/200/250/tr/photos/nastygal/07d0e3b7/nasty-gal-designer-green-Satin-Balloon-Sleeve-Wrap-Maxi-Dress.jpeg",
+    price: "$94 $66 (30% off)",
+    logo: "Nasty Gal",
+  },
+  {
+    name: "NASTY GAL",
+    desc: "Satin Ballon Sleeve Wrap Maxi Dress Green",
+    img: "https://cdna.lystit.com/200/250/tr/photos/nastygal/07d0e3b7/nasty-gal-designer-green-Satin-Balloon-Sleeve-Wrap-Maxi-Dress.jpeg",
+    price: "$94 $66 (30% off)",
+    logo: "Nasty Gal",
+  },
+];
+
+let cont = document.getElementById("cont");
+
+// let filter = document.getElementById("filter");
+
+let cartLS = JSON.parse(localStorage.getItem("cart-page")) || [];
+
+function Display(data) {
+  cont.innerHTML = "";
+  data.forEach(function (ele) {
+    // e.preventDefault()
+
+    let product = document.createElement("div");
+
+    let img = document.createElement("img");
+    img.src = ele.img;
+
+    let name1 = document.createElement("h1");
+    name1.textContent = ele.name;
+
+    let desc = document.createElement("h2");
+    desc.textContent = ele.desc;
+
+    let price = document.createElement("h2");
+    price.textContent = ele.price;
+
+    let logo = document.createElement("h2");
+    logo.textContent = ele.logo;
+
+    let btn = document.createElement("button");
+    btn.innerText = "Add to cart";
+    btn.addEventListener("click", function () {
+      AddtoCart(ele);
+    });
+
+    product.append(img, name1, desc, price, logo,btn);
+    cont.append(product);
+  });
+}
+Display(product_data);
+// filter.addEventListener("change", function () {
+//   if (filter.value == "") {
+//     Display(product_data);
+//   } else {
+//     let filtered = product_data.filter(function (ele) {
+//       return ele.catagory === filter.price;
+//     });
+//     Display(filtered);
+//   }
+// });
+
+// function AddtoCart(product) {
+//   let iscartadded = false;
+
+//   for (let i = 0; i < cartLS.length; i++) {
+//     if (cartLS[i].productID === product.productID) {
+//       alert("product Alreay in the Cart");
+//       iscartadded = true;
+//       break;
+//     }
+//   }
+//   if (iscartadded == false) {
+//     cartLS.push(product);
+//     alert("product Added Succesfully");
+//     //  console.log(cartLS)
+//     localStorage.setItem("cart-page", JSON.stringify(cartLS));
+//   }
+// }
+
+// let signinLS = JSON.parse(localStorage.getItem("signin"));
+
+// let nav = document.getElementById("nav");
+
+// if (signinLS != null) {
+//   nav.innerHTML = null;
+
+//   let username = document.createElement("h1");
+//   username.innerText = signinLS.name;
+
+//   let cartbtn = document.createElement("a");
+//   cartbtn.setAttribute("href", "cart.html");
+//   cartbtn.innerText = "CART";
+
+//   nav.append(username, cartbtn);
+//}
